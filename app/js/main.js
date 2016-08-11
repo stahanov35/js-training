68ee94b185d23cffeb637c6f5be0bc6685fe28a6
Denis Kolesnik <denis.kolesnik@dev-pro.net>
2016-08-11 19:00:20 +0300

    Add triangel loop

--- app/js/main.js
+++ app/js/main.js
@@ -3,6 +3,14 @@
 
 document.onreadystatechange = () => {
   if (document.readyState === 'complete') {
+    let i = 0,
+      str = '';
+
+    while(i < 7){
+      str += '#';
+      i++;
+      console.log(str);
+    }
   }
 };
 }());
