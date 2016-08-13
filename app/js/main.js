534bab8275466be18cc71a03c5b8454b1b0739b7
denis.kolesnik <denis.kolesnik@dev-pro.net>
2016-08-13 19:13:28 +0300

    Add realization with FizzBuzz string

--- app/js/main.js
+++ app/js/main.js
@@ -3,6 +3,23 @@
 
 document.onreadystatechange = () => {
   if (document.readyState === 'complete') {
+    for (let i = 1; i <= 100; i++) {
+      let remainderDiv3 = i % 3;
+      let remainderDiv5 = i % 5;
+
+      if (!remainderDiv3 && !remainderDiv5) {
+        console.log('FizzBuZZ');
+      }
+      if (!remainderDiv3) {
+        console.log('Fizz');
+        continue;
+      }
+      if (!remainderDiv5) {
+        console.log('Buzz');
+        continue;
+      }
+      console.log(i);
+    }
   }
 };
 }());
