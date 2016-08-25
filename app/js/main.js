09d6744c6289945a69b8e0aa1e0abd9e50ed542e
denis.kolesnik <denis.kolesnik@dev-pro.net>
2016-08-25 23:57:42 +0300

    Duff's device implementation for loop

--- app/js/main.js
+++ app/js/main.js
@@ -3,42 +3,54 @@
 
 document.onreadystatechange = () => {
   if (document.readyState === 'complete') {
-    let toValue = 7;
-    let count = Math.ceil(toValue / 8);
-    console.log(count);
+    let toValue = 17;
+    let count = Math.floor(toValue / 8);
     let i = toValue % 8;
     let j = 1;
-    console.log(i);
     do {
       switch (i) {
-        case 1:
-          i++;
-          console.log(i + j);
-        case 2:
-          i++;
-          console.log(i + j);
-        case 3:
-          i++;
-          console.log(i + j);
-        case 4:
-          i++;
-          console.log(i + j);
-        case 5:
-          i++;
-          console.log(i + j);
-        case 6:
-          i++;
-          console.log(i + j);
-        case 7:
-          i++;
-          console.log(i + j);
         case 8:
           i++;
-          console.log(i + j);
-        default: i = 1;
+          print(j++);
+        case 7:
+          i++;
+          print(j++);
+        case 6:
+          i++;
+          print(j++);
+        case 5:
+          i++;
+          print(j++);
+        case 4:
+          i++;
+          print(j++);
+        case 3:
+          i++;
+          print(j++);
+        case 3:
+          i++;
+          print(j++);
+        case 1:
+          i++;
+          print(j++);
+        default: i = 8;
       }
-      j++;
-    } while (j <= count + 1);
+    } while (count--);
+  }
+
+  function print(number) {
+    let devRemainder3 = number % 3;
+    let devRemainder5 = number % 5;
+
+    if (!devRemainder3 && !devRemainder5) {
+      console.log('FizzBuzz');
+    } else if (!devRemainder3) {
+      console.log('Fizz');
+    } else if (!devRemainder5) {
+      console.log('Buzz');
+    } else{
+      console.log(number);
+    }
   }
 };
 }());
