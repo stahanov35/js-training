dcaf00f0fd92587e4bb269728cb34b0f5157516e
Denis Kolesnik <denis.kolesnik@dev-pro.net>
2016-08-29 21:24:25 +0300

    Add Range function with console.log

--- app/js/main.js
+++ app/js/main.js
@@ -3,6 +3,20 @@
 
 document.onreadystatechange = () => {
   if (document.readyState === 'complete') {
+
+    range(1,10);
+
+    function range(a, b) {
+      let arr = [];
+      let count = Math.abs(b - a) + 1;
+
+      for (; count; count--) {
+        arr.push(a++);
+      }
+
+      console.log(arr);
+      return arr;
+    }
   }
 };
 }());
