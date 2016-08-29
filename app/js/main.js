bddbbaa98e1b60900a71763cec83051be9b44873
Denis Kolesnik <denis.kolesnik@dev-pro.net>
2016-08-29 18:22:41 +0300

    Add loop with definited value

--- app/js/main.js
+++ app/js/main.js
@@ -3,6 +3,23 @@
 
 document.onreadystatechange = () => {
   if (document.readyState === 'complete') {
+    let boardSize = 8;
+    let boardRowFirst = '';
+    let boardRowSecond = '';
+    let result = '';
+
+    // Create templates for rows
+    for (let i = boardSize; i; i -= 2) {
+      boardRowFirst += ' #';
+      boardRowSecond += '# ';
+    }
+
+    // Generate final result
+    for (; boardSize; boardSize--) {
+      result += boardSize%2 ? `${boardRowFirst}\n` : `${boardRowSecond}\n`;
+    }
+
+    console.log(result);
   }
 };
 }());
